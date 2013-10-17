b20456d232c8561eddd37fee2ac502e9671f2eb4
Sunil Pai <threepointone@gmail.com>
2013-10-17 12:51:54 +0530

    first commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+
+# progressify
+
+####  (work in progress)
+
---
+++ index.js
@@ -0,0 +1,41 @@
+// for a function that accepts a callback as its last paramater, 
+// this function makes it so that a progress bar appears (and then disappears) whenever that function is called
+
+
+"use strict";
+
+var slice = [].slice;
+
+function progressify(fn, scope){
+	return function(){
+		var bar = require('progress')();
+		bar.inc();
+		var intval = setInterval(function(){
+			if(bar.finished){
+				clearInterval(intval);
+			}
+			else{
+				bar.inc();
+			}
+		}, 1000);
+
+		var args = slice.call(arguments, 0);
+		var done = args[args.length-1];
+		if(typeof done !== 'function'){
+			done = function(){
+				bar.end();
+			};
+			args = args.concat([done]);
+		}
+		else{
+			// couch the function to add our call to end the progress bar
+			args[args.length-1] = function(){
+				bar.end();
+				done.apply(null, arguments);
+			};
+		}
+		return fn.apply(scope, args);
+	};
+}
+
+module.exports = progressify;
